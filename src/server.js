9834f7e73206ff459e06d0369ffa1592765f8ae9
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-11-26 17:46:22 +0300

    feat: add cookie-parser middleware for enhanced session management

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { env } from './utils/env.js';
 
@@ -15,8 +16,8 @@ export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     pino({
