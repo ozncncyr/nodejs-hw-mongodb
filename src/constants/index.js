e9d657650e134b597addb5de335a6a9e041b517b
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-12-03 17:28:30 +0300

    feat: add SMTP configuration and email sending functionality

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,3 +7,11 @@ export const TIMER = {
   FIFTEEN_MINUTES: 15 * 60 * 1000,
   ONE_DAY: 24 * 60 * 60 * 1000,
 };
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,15 @@
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
 import createHttpError from 'http-errors';
+import jwt from 'jsonwebtoken';
 
-import { TIMER } from '../constants/index.js';
+import { env } from '../utils/env.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 import { UsersCollection } from '../db/models/user.js';
 import { SessionsCollection } from '../db/models/session.js';
+
+import { TIMER, SMTP } from '../constants/index.js';
 
 export const registerUser = async payload => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -92,6 +96,21 @@ export const requestResetToken = async email => {
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    }
+  );
 
-  //bunu birazdan tamamlayacağız
+  await sendEmail({
+    from: env(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  });
 };
